92394a8846024624d19649f336e9a23e79b6de6c
tringocao <tringocao@gmail.com>
2019-03-26 23:26:29 +0700

    ,

--- client/js/script.js
+++ client/js/script.js
@@ -39,6 +39,39 @@ COMMON_TEMPLATE= {
                         </div>
                     </div>
                 </div>
+				
+				
+									<!-- Simulate a smartphone / tablet -->
+					<div class="mobile-container">
+
+					<!-- Top Navigation Menu -->
+					<div class="topnav">
+					  <a href="#home" class="active">Logo</a>
+					  <div id="myLinks">
+						<a href="#news">News</a>
+						<a href="#contact">Contact</a>
+						<a href="#about">About</a>
+					  </div>
+					  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+						<i class="fa fa-bars"></i>
+					  </a>
+					</div>
+
+					<!-- End smartphone / tablet look -->
+					</div>
+
+					<script>
+					function myFunction() {
+					  var x = document.getElementById("myLinks");
+					  if (x.style.display === "block") {
+						x.style.display = "none";
+					  } else {
+						x.style.display = "block";
+					  }
+					}
+					</script>
+				
+				
                 <div class="bottom-wrapper">
                     <div class="container bottom">
                         <div class="nav-bar-wrapper">
