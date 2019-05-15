0f578ff995180a7ed293dbf55f055818346620c3
tuanpq2 <tuanpq2@vng.com.vn>
2019-05-15 16:11:08 +0700

    Update utf8

--- client/chinhsuabaiviet.php
+++ client/chinhsuabaiviet.php
@@ -5,6 +5,7 @@
 	$password = "";
 	$dbname = "ass2";
 	$conn = new mysqli($servername, $username, $password, $dbname);
+	mysqli_set_charset($conn, 'UTF8');
 
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
--- client/thembaiviet.php
+++ client/thembaiviet.php
@@ -6,6 +6,7 @@
 		$password = "";
 		$dbname = "ass2";
 		$conn = new mysqli($servername, $username, $password, $dbname);
+		mysqli_set_charset($conn, 'UTF8');
 
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
@@ -128,7 +129,6 @@
 		});
 
 		$(function() {
-			console.log("abc");
 
 			$(".Editor-editor").on("keyup", function() {
 				var content = $(this).text();
